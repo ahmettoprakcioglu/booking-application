a5bb36f3cfb682404503858ee91d52832a2eee1f
Ahmet Kaan Toprakçıoğlu <56438853+ahmettoprakcioglu@users.noreply.github.com>
2024-07-14 13:05:45 +0300

    feat: added toast message

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from '../../utils/helpers';
 import { number, shape, string } from 'prop-types';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { deleteCabin } from '../../services/apiCabins';
+import toast from 'react-hot-toast';
 
 const TableRow = styled.div`
   display: grid;
@@ -51,12 +52,12 @@ const CabinRow = ({ cabin }) => {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: id =>deleteCabin(id),
     onSuccess: () => {
-      alert('Cabin successfully deleted')
+      toast.success('Cabin successfully deleted');
       queryClient.invalidateQueries({
         queryKey: ['cabins']
       })
     },
-    onError: err => alert(err.message)
+    onError: err => toast.error(err.message)
   });
 
   return (
